f40693fc30eb32a0302052c19f8c5c239879126c
WoonHak Kong <woonhak.kong@georgebrown.ca>
2022-10-04 19:32:06 -0400

    Buttons are implemented

---
+++ Assets/[Scripts]/MainSceneButtonManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class MainSceneButtonManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+        EditorSceneManager.LoadScene("PlayScene",UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+
+    public void OnClickContinue()
+    {
+        EditorSceneManager.LoadScene("PlayScene", UnityEngine.SceneManagement.LoadSceneMode.Single);
+    }
+    
+    public void OnClickCredit()
+    {
+
+    }
+
+    public void OnClickExit()
+    {
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+
+    }
+
+}
